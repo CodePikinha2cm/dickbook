abc8948ed5248bf18a4af03557983652e1866169
CodePikinha2cm <hospedagenss@gmail.com>
2023-10-07 14:27:18 -0300

    socorro deus

--- src/View.php
+++ src/View.php
@@ -8,8 +8,21 @@ class View
 
     public function run()
     {
+        match ($this->path) {
+            'login' => $this->semLayout(),
+            default => $this->comLayout()
+        };
+    }
+
+    private function comLayout()
+    {
         require_once __DIR__ . "../../Layout/header.php";
         require_once "./views/{$this->path}/index.php";
         require_once __DIR__ . "../../Layout/footer.php";
+    }
+
+    private function semLayout()
+    {
+        require_once "./views/{$this->path}/index.php";
     }
 }
